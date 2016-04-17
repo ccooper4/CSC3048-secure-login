b1d615d08f5c978c53e189be58efa37d7b2ac5f7
Rory Powell <rpowell03@qub.ac.uk>
2016-04-17 22:55:51 +0100

    Moving number gen util, removing unneccessary imports in policy.fxml, modifying AES

--- shared/src/main/java/cipher/util/AESKeyGenerator.java
+++ shared/src/main/java/cipher/util/AESKeyGenerator.java
@@ -1,6 +1,6 @@
 package cipher.util;
 
-import static cipher.util.RandomNumberGen.getRandom;
+import static util.RandomNumberGen.getRandom;
 
 /**
  *  Utility class for generating a random key to be used for AES encryption
--- shared/src/main/java/util/RandomNumberGen.java
+++ shared/src/main/java/util/RandomNumberGen.java
@@ -1,4 +1,4 @@
-package cipher.util;
+package util;
 
 import java.util.Random;
 
