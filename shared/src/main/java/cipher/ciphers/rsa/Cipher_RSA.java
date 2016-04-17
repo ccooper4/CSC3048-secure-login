2142d0e43199dbf872b7532940a1be470a5ee5b7
Andrew Fletcher <afletcher04@qub.ac.uk>
2016-04-17 20:44:50 +0100

    Finishing decryption in RSA, fixing bug in applyExponentiationBySquaringDivision

--- shared/src/main/java/cipher/ciphers/rsa/Cipher_RSA.java
+++ shared/src/main/java/cipher/ciphers/rsa/Cipher_RSA.java
@@ -12,12 +12,12 @@ public class Cipher_RSA extends BaseCipher {
     //region Fields
 
     /**
-     * The P value for RSA.
+     * The P value for RSA. - Chose a larger value of P to handle a larger number range.
      */
     private static final int p = 257;
 
     /**
-     * The Q value for RSA.
+     * The Q value for RSA. - Chose a larger value of Q to handle a larger number range.
      */
     private static final int q = 337;
 
@@ -44,40 +44,42 @@ public class Cipher_RSA extends BaseCipher {
     /**
      * The character map.
      */
-    private static final Map<Character , Integer> characterMap = new HashMap<Character, Integer>() {{
-        put(' ', 0);
-        put('A', 1);
-        put('B', 2);
-        put('C', 3);
-        put('D', 4);
-        put('E', 5);
-        put('F', 6);
-        put('G', 7);
-        put('H', 8);
-        put('I', 9);
-        put('J', 10);
-        put('K', 11);
-        put('L', 12);
-        put('M', 13);
-        put('N', 14);
-        put('O', 15);
-        put('P', 16);
-        put('Q', 17);
-        put('R', 18);
-        put('S', 19);
-        put('T', 20);
-        put('U', 21);
-        put('V', 22);
-        put('W', 23);
-        put('X', 24);
-        put('Y', 25);
-        put('Z', 26);
+    private static final Map<Character , String> characterMap = new HashMap<Character, String>() {{
+        put(' ', "00");
+        put('A', "01");
+        put('B', "02");
+        put('C', "03");
+        put('D', "04");
+        put('E', "05");
+        put('F', "06");
+        put('G', "07");
+        put('H', "08");
+        put('I', "09");
+        put('J', "10");
+        put('K', "11");
+        put('L', "12");
+        put('M', "13");
+        put('N', "14");
+        put('O', "15");
+        put('P', "16");
+        put('Q', "17");
+        put('R', "18");
+        put('S', "19");
+        put('T', "20");
+        put('U', "21");
+        put('V', "22");
+        put('W', "23");
+        put('X', "24");
+        put('Y', "25");
+        put('Z', "26");
     }};
 
     //region Abstract class override methods.
 
     @Override
     public String encrypt(String plaintext) {
+
+        plaintext = plaintext.toUpperCase();
 
         String output = "";
 
@@ -93,31 +95,61 @@ public class Cipher_RSA extends BaseCipher {
             char blockChar1 = plaintext.charAt(i);
             char blockChar2 = plaintext.charAt(i+1);
 
-            int blockNum1 = convertCharToNumber(blockChar1);
-            int blockNum2 = convertCharToNumber(blockChar2);
+            String blockNum1 = convertCharToNumber(blockChar1);
+            String blockNum2 = convertCharToNumber(blockChar2);
 
-            String fullBlock = Integer.toString(blockNum1) + Integer.toString(blockNum2);
+            String fullBlock = blockNum1 + blockNum2;
 
             long numBlock = Long.parseLong(fullBlock);
 
             long cipher = applyExponentiationBySquaringDivision(numBlock, e, n);
 
-            long msg = applyExponentiationBySquaringDivision(cipher, d, n);
-
-            output += Long.toString(cipher);
+            output += Long.toString(cipher) + " ";
 
         }
 
         logEncryption(plaintext, output);
-        return null;
+
+        output = output.trim();
+
+        return output;
     }
 
     @Override
     public String decrypt(String encryptedText) {
-        String output = null;
 
-        logDecryption(encryptedText, output);
-        return null;
+        String plainText = "";
+
+        e = calculateEFromEuclidGcd(d, w, w);
+
+        String[] cipherTextBlocks = encryptedText.split("\\ ");
+
+        for (String block : cipherTextBlocks) {
+
+            long numberBlock = Long.parseLong(block);
+
+            long message = applyExponentiationBySquaringDivision(numberBlock, d, n);
+
+            String textBlock = Long.toString(message);
+
+            textBlock = String.format("%04d", Long.parseLong(textBlock));
+
+            String blockPart1 = textBlock.substring(0, 2);
+            String blockPart2 = textBlock.substring(2, 4);
+
+            char blockPart1Char = convertNumberToChar(blockPart1);
+            char blockPart2Char = convertNumberToChar(blockPart2);
+
+            String finalMessageBlock = Character.toString(blockPart1Char) + Character.toString(blockPart2Char);
+
+            plainText += finalMessageBlock;
+
+            int x = 3;
+
+        }
+
+        logDecryption(encryptedText, plainText);
+        return plainText;
     }
 
     //endregion
@@ -131,18 +163,20 @@ public class Cipher_RSA extends BaseCipher {
      * @param n The n number from the equation.
      * @return
      */
-    private long applyExponentiationBySquaringDivision(long num, long exponent, long n) {
+    public long applyExponentiationBySquaringDivision(long num, long exponent, long n) {
 
         char[] binaryExponentChars = Long.toBinaryString(exponent).toCharArray();
 
         long c = 1;
 
-        for (int i = binaryExponentChars.length - 1; i >= 0; i--)
+        for (int i = 0; i < binaryExponentChars.length; i++)
         {
-            c = (c * c) % n;
+            long tmp = c * c;
+            c = tmp % n;
 
             if (binaryExponentChars[i] == '1') {
-                c = (c * num) % n;
+                long tmp2 = c * num;
+                c = tmp2 % n;
             }
         }
 
@@ -154,16 +188,10 @@ public class Cipher_RSA extends BaseCipher {
      * @param individualCharacter The character.
      * @return A numerical representation of the character.
      */
-    private int convertCharToNumber(char individualCharacter) {
+    private String convertCharToNumber(char individualCharacter) {
 
-        if (characterMap.containsKey(individualCharacter)) {
-            return characterMap.get(individualCharacter);
-        }
-        else
-        {
-            //Return a standard ASCII number.
-            return (int)individualCharacter;
-        }
+        return characterMap.get(individualCharacter);
+
     }
 
     /**
@@ -171,17 +199,18 @@ public class Cipher_RSA extends BaseCipher {
      * @param individualCharacter The character.
      * @return A numerical representation of the character.
      */
-    private char convertNumberToChar(int individualCharacter) {
+    private char convertNumberToChar(String individualCharacter) {
 
-        for (Map.Entry<Character,Integer> entry : characterMap.entrySet()) {
+        char rtr = ' ';
 
-            if (entry.getValue() == individualCharacter) {
-                return entry.getKey();
+        for (Map.Entry<Character,String> entry : characterMap.entrySet()) {
+
+            if (entry.getValue().equals(individualCharacter)) {
+                rtr =  entry.getKey();
             }
         }
 
-        //Otherwise, map back via ASCII
-        return (char)individualCharacter;
+        return rtr;
     }
 
     /**
@@ -190,7 +219,7 @@ public class Cipher_RSA extends BaseCipher {
      * @param b The second number.
      * @return The greatest common divider of A and B.
      */
-    private int calculateEFromEuclidGcd(int a, int b, int w) {
+    public int calculateEFromEuclidGcd(int a, int b, int w) {
 
         int prevAi = 1;
         int ai = 0;
--- shared/src/test/java/cipher/ciphers/rsa/RSATest.java
+++ shared/src/test/java/cipher/ciphers/rsa/RSATest.java
@@ -1,7 +1,10 @@
 package cipher.ciphers.rsa;
 
 import cipher.ciphers.BaseCipherTest;
+import junit.framework.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RSATest extends BaseCipherTest {
 
@@ -9,15 +12,67 @@ public class RSATest extends BaseCipherTest {
         cipher = new Cipher_RSA();
     }
 
+    //region Main Crypto Tests
+
     @Test
     @Override
     public void testEncryption() {
-//        testEncryption("ZZ", "expected result");
+        testEncryption("markfrequency", "80200 20944 24574 4979 54423 60810 59202");
     }
 
     @Test
     @Override
     public void testDecryption() {
-//        testDecryption("cipher text", "expected result");
+        testDecryption("80200 20944 24574 4979 54423 60810 59202", "MARKFREQUENCY ");
     }
+
+    //endregion
+
+    //region RSA Component Tests
+
+    @Test
+    public void testExponentiationBySquaringDivision() {
+
+        long num = 21;
+        long exp = 7;
+        long n = 133;
+
+        Cipher_RSA rsaCipher = (Cipher_RSA)cipher;
+
+        long res = rsaCipher.applyExponentiationBySquaringDivision(num, exp, n);
+
+        assertEquals(14, res);
+    }
+
+    @Test
+    public void testExponentiationBySquaringDivisionLargerPQ() {
+
+        //C = M^E mod(n)
+        long num = 131;
+        long exp = 65777;
+        long n = 86609;
+
+        Cipher_RSA rsaCipher = (Cipher_RSA)cipher;
+
+        long res = rsaCipher.applyExponentiationBySquaringDivision(num, exp, n);
+
+        assertEquals(70695, res);
+    }
+
+    @Test
+    public void testCalculateEFromEuclidGcd() {
+
+        int d = 31;
+        int w = 108;
+
+        Cipher_RSA rsaCipher = (Cipher_RSA)cipher;
+
+        int e = rsaCipher.calculateEFromEuclidGcd(d,w,w);
+
+        assertEquals(7, e);
+
+    }
+
+
+    //endregion
 }
